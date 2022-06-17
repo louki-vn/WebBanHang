e556d5447b8763ab5056200804338f9e779d5f61
DuongTuyen18 <dvt18012000@gmail.com>
2022-06-17 20:22:20 +0700

    tuyen

--- BanHang/WebShop/Areas/Admin/Controllers/CategoryController.cs
+++ BanHang/WebShop/Areas/Admin/Controllers/CategoryController.cs
@@ -18,7 +18,7 @@ namespace WebShop.Areas.Admin.Controllers
             ViewBag.user_logined = Session["user_logined"];
             ViewBag.is_logined = Session["is_logined"];
             var result = db.CATEGORies.ToList();
-            return Json(result);
+            return View(result);
         }
 
         [HttpPost]
