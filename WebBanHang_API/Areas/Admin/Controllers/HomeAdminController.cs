e018d35e33cbb582e5acbbd6aace7c9531ff2f28
DuongTuyen18 <dvt18012000@gmail.com>
2022-06-18 10:49:49 +0700

    tuyen

--- WebBanHang_API/Areas/Admin/Controllers/HomeAdminController.cs
+++ WebBanHang_API/Areas/Admin/Controllers/HomeAdminController.cs
@@ -16,16 +16,61 @@ namespace WebBanHang_API.Areas.Admin.Controllers
             db.Configuration.ProxyCreationEnabled = false;
         }
         [HttpGet]
-        [Route("api/admin/get_count_member")]
-        public IHttpActionResult getCountMember()
+        [Route("api/admin/get_count_HomeAdmin")]
+        public IHttpActionResult getCountHomeAdmin()
         {
-            var mem = db.MEMBERs.ToList().Count();
-            var order = db.Database.SqlQuery<TRANSACTION>("SelectFinishTransaction").ToList().Count();
-            return Ok(new { data=mem, order });
+            var mem = db.MEMBERs.ToList();
+            var order = db.Database.SqlQuery<TRANSACTION>("SelectFinishTransaction").ToList();
+            var topproduct = db.PRODUCTs.SqlQuery("exec SelectTopProduct").ToList();
+            var category = db.CATEGORies.ToArray();
+            var count_mem = mem.Count();
+            var count_order = order.Count();
+            var item = db.ITEM_SOLD.ToList();
+            int amount = 0, total = 0;
+
+            foreach (var o in order)
+            {
+                total += o.amount;
+            }
+
+            foreach (var o in item)
+            {
+                amount += (int)o.qty;
+
+            }
+            Dictionary<int, string> category_count = new Dictionary<int, string>();
+            foreach (var c in category)
+            {
+                category_count.Add(c.category_id, c.name);
+            }
+            var brand = db.BRANDs.ToList();
+            Dictionary<int, string> brand_count = new Dictionary<int, string>();
+            foreach (var i in brand)
+            {
+                brand_count.Add(i.brand_id, i.brand_name);
+            }
+            var topmem = db.Database.SqlQuery<Mem_Cart>("exec SelectTopMember").ToList();
+            return Ok(new { data= count_mem, count_order, amount, total});
             //return con.THEMTHONGTINDANGKYCLVUKHITAU(id, mc) ?
             //    Request.CreateResponse(HttpStatusCode.Created, mc)
             //    : Request.CreateResponse(HttpStatusCode.BadRequest);
 
+        }
+        [HttpGet]
+        [Route("api/admin/get_topProduct_HomeAdmin")]
+        public IHttpActionResult getTopProductHomeAdmin()
+        {
+            var topproduct = db.PRODUCTs.SqlQuery("exec SelectTopProduct").ToList();
+           
+            return Ok(new { data = topproduct });
+        }
+        [HttpGet]
+        [Route("api/admin/get_topMember_HomeAdmin")]
+        public IHttpActionResult getTopMemBerHomeAdmin()
+        {
+           
+            var topmem = db.Database.SqlQuery<Mem_Cart>("exec SelectTopMember").ToList();
+            return Ok(new { data = topmem });
         }
     }
 }
