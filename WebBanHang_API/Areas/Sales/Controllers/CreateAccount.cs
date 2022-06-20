acc4fd4b7f2ea10da665f1f4da3fdd8ebb54dd9a
CattyTN <70119817+CattyTN@users.noreply.github.com>
2022-06-20 15:43:57 +0700

    update API DangKi by Nhat

--- WebBanHang_API/Areas/Sales/Controllers/CreateAccount.cs
+++ WebBanHang_API/Areas/Sales/Controllers/CreateAccount.cs
@@ -1,1 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebBanHang_API.Models;
+
+namespace WebBanHang_API.Areas.Sales.Controllers
+{
+    [Route("api/createaccount")]
+    public class CreateAccountController : ApiController
+    {
+        Shop db = new Shop();
+        
+        [HttpPost]
+        [Route("api/createaccount/addmember/{username}/{name}/{password}/{phonenumber}/{address}")]
+        public IHttpActionResult Add_Member(string username, string name, string password, string phonenumber, string address)
+        {
+            int success = 0;
+            var u = new SqlParameter("@username", username);
+            var result = db.Database.SqlQuery<MEMBER>("exec getMEMBERfromusername @username", u).ToList();
+            int check = result.Count();
+            if(check == 0)
+            {
+                var username2var = new SqlParameter("@username", username);
+                var passvar = new SqlParameter("@password", Data.MD5Hash(password));
+                var namevar = new SqlParameter("@name", name);
+                var phonevar = new SqlParameter("@phone", phonenumber);
+                var addressvar = new SqlParameter("@address", address);
+                db.Database.ExecuteSqlCommand("exec AddMember @name, @username, @password, @phone, @address",
+                                                            namevar, username2var, passvar, phonevar, addressvar);
+                success = 1;
+            }
+            return Json(success);
+        }
+    }
+}
 
